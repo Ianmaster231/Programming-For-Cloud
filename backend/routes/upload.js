d08008997e04ea8b3f7615870d085919bef4a4d3
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2022-04-16 16:24:04 +0200

    Testing base code 64

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -68,17 +68,7 @@ upload.route("/").post(imageUpload.single("image"), (req, res) => {
       uploadToCloud("pending/", req.file).then(([r]) =>{
         console.log(r.metadata.mediaLink);
 
-        imageToBase64("pending/", req.file) // Path to the image
-        .then(
-            (response) => {
-                console.log(response); // "cGF0aC90by9maWxlLmpwZw=="
-            }
-        )
-        .catch(
-            (error) => {
-                console.log(error); // Logs an error if there was one
-            }
-        )
+       
         publicMessage({
           email:email,
           filename: req.file.originalname,
@@ -87,7 +77,77 @@ upload.route("/").post(imageUpload.single("image"), (req, res) => {
         });
 
 
-       
+        var img2data = (function() {
+          'use strict';
+          return {
+            // this.qS(foo)
+            qS: function(el) {
+              return document.querySelector(el);
+            },
+            run: function() {
+              this.convert(); 
+            },
+            convert: function() {
+              // vars 
+              var fls = this.qS('#files'),
+                  output = this.qS('.output'),
+                  overlay = this.qS('.overlay'),
+                  close_overlay = this.qS('.close_overlay');
+              
+              fls.addEventListener('change', function(e) {
+                var file = fls.files[0],
+                    txtType = /text.*/, // all text files
+                    imgType = /image.*/, // all image files
+                    fR = new FileReader(); // fileReader start
+                
+                fR.onload = function(e) {
+                  // if text 
+                  if (file.type.match(txtType)) {
+                    var rS = fR.result,
+                        // template 
+                        render = '<a class="btn" href="'+rS+'" target="blank">Output</a><ul>'+
+                          '<li><b>Name: </b>  '+file.name+'</li>'+
+                          '<li><b>Size: </b>  '+file.size+'</li>'+
+                          '<li><b>Type: </b>  '+file.type+'</li>'+
+                          '<li><b>Data uri: </b></li>'+
+                        '</ul>'+
+                        '<pre class="textarea">'+rS+'</pre>';
+                    output.innerHTML = render; 
+                  // if image
+                  } else if(file.type.match(imgType)) {
+                    var rS2 = fR.result,
+                        // template
+                        tmpl = '<a class="btn" href="'+rS2+'" target="blank">Output</a>'+
+                        '<img class="thumb" src="'+rS2+'" alt="'+file.name+'"><ul>'+
+                          '<li><b>Name: </b>  '+file.name+'</li>'+
+                          '<li><b>Size: </b>  '+file.size+'</li>'+
+                          '<li><b>Type: </b>  '+file.type+'</li>'+
+                          '<li><b>Data uri: </b></li>'+
+                        '</ul>'+
+                        '<pre class="textarea">'+rS2+'</pre>';
+                    output.innerHTML = tmpl;
+                   // if not support
+                  }else{
+                    output.innerHTML = '<h1>Maaf file yang anda upload tidak mendukung</h1>';
+                  }
+                };
+                
+                // on loaded add events
+                fR.onloadend = function(e) {
+                  overlay.classList.add('show'); // add class
+                  close_overlay.onclick = function(){
+                    overlay.classList.remove('show'); // remove class
+                    fls.value = ''; // remove files
+                  };
+                };  
+                // convert to data uri
+                fR.readAsDataURL(file); 
+              });
+            }
+          };
+        })();
+        
+        img2data.run();
 
   
   if (req.file) {
