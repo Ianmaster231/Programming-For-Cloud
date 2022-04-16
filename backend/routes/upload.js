5619cbad86331a7697ea9bb8241052c82e8ef1f2
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2022-04-16 18:54:08 +0200

    test 14

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -78,18 +78,18 @@ upload.route("/").post(imageUpload.single("image"), (req, res) => {
 
         
 
-        // function to encode file data to base64 encoded string
-        function base64_encode(file) {
-            // read binary data
-            var bitmap = fs.readFileSync(file);
-            // convert binary data to base64 encoded string
-            return new Buffer(bitmap).toString('base64');
-        }
-        var base64str = base64_encode(req.file);
+       
   
   if (req.file) {
     console.log("File downloaded at: " + req.file.path);
-  
+   // function to encode file data to base64 encoded string
+   function base64_encode(file) {
+    // read binary data
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+}
+var base64str = base64_encode(req.file);
     //imageToBase64("ianzammit.me");
         //or
         //import imageToBase64 from 'image-to-base64/browser';
