add09f1a89a98ceefd3aa7e50ad51ff2c2e9c5dd
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2022-04-16 14:09:03 +0200

    Testing base64 convert

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -74,13 +74,42 @@ upload.route("/").post(imageUpload.single("image"), (req, res) => {
           url: r.metadata.mediaLink,
           date: new Date().toUTCString(),
         });
-      
+
+       
+        if (window.File && window.FileReader && window.FileList && window.Blob) {
+          document.getElementById('fileInput').addEventListener('change', handleFileSelect, false);
+        } else {
+          alert('The File APIs are not fully supported in this browser.');
+        }
+        
+        function handleFileSelect(evt) {
+          var f = evt.target.files[0]; // FileList object
+          var reader = new FileReader();
+          // Closure to capture the file information.
+          reader.onload = (function(theFile) {
+            return function(e) {
+              var binaryData = e.target.result;
+              //Converting Binary Data to base 64
+              var base64String = window.btoa(binaryData);
+              //showing file converted to base64
+              document.getElementById('base64').value = base64String;
+              alert('File converted to base64 successfuly!\nCheck in Textarea');
+            };
+          })(f);
+          // Read in the image file as a data URL.
+          reader.readAsBinaryString(f);
+        }
+
+
+
   
-  //if (req.file) {
-   // console.log("File downloaded at: " + req.file.path);
+  if (req.file) {
+    console.log("File downloaded at: " + req.file.path);
+ 
     //Upload to google cloud
     //Convert to base64
     //Send to PDF Conversion API
+  }
    res.send({
      status: "200",
     message: "File uploaded successfully! Processing..",
