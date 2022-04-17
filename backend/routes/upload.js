14937f56abc3ea0a5e841615d988d2ead8738d16
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2022-04-17 20:01:32 +0200

    chaning api key

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -109,7 +109,7 @@ upload.route("/").post(imageUpload.single("image"), (req, res) => {
   if (req.file) {
     console.log("File downloaded at: " + req.file.path);
     const data = {
-      "api_key": "97b5d164e4324c621c17865d6d8ee1dd6df6852dcf671da22b6710a40aecc425",           // string, required
+      "api_key": "fa01e72a13307deb7c20217a29074a9544c1edaf9e0cf14d15d348b965c9a310",           // string, required
       "image": "data:image/png;base64,"+base64str,            // string, required
       "transparent_color": "#ffffff" // string, optional, default:#ffffff
     };
