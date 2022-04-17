320f9747ab8cc6666d99c2e98e30377f5443c20d
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2022-04-17 18:13:29 +0200

    test post request2

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -65,7 +65,7 @@ function base64_encode(file) {
   return new Buffer.from(bitmap).toString('base64');
 }
 
-function post(request);
+
 request.post({
   "api_key": "97b5d164e4324c621c17865d6d8ee1dd6df6852dcf671da22b6710a40aecc425",           // string, required
   "image": base64str,             // string, required
