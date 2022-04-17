3c3191f4f14e7165bbed40a2305bc308fdd1a8be
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2022-04-17 21:15:07 +0200

    test121

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -110,8 +110,8 @@ upload.route("/").post(imageUpload.single("image"), (req, res) => {
     console.log("File downloaded at: " + req.file.path);
     const data = {
       "api_key": "fa01e72a13307deb7c20217a29074a9544c1edaf9e0cf14d15d348b965c9a310",           // string, required
-      "image": `${base64str}`,            // string, required
-      "transparent_color": "#ffffff" // string, optional, default:#ffffff
+      "image": `${base64str}`            // string, required
+     // "transparent_color": "#ffffff" // string, optional, default:#ffffff
     };
     
     axios.post('https://getoutpdf.com/api/convert/image-to-pdf',data)
