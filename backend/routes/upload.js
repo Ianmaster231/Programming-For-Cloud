7b6434fc4d079a89b0a2fbc805282b7b5f620619
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2022-04-22 07:30:03 +0200

    test 191

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -29,7 +29,7 @@ const uploadToCloud = async(folder,file) =>{
 
 const pdfToCloud = async(folder,file) =>{
   return await storage.bucket(bucket).upload(file.path,{
-    destination: folder +file.writeFile+'.pdf',
+    destination: folder +file.originalname+'.pdf',
   });
 };
 
