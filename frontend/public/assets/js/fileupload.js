8c90adc0f16939c963b3b2033385a931be2a22cc
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2022-04-29 11:25:46 +0200

    download button try 1

--- frontend/public/assets/js/fileupload.js
+++ frontend/public/assets/js/fileupload.js
@@ -13,17 +13,25 @@ const uploadFile = async () => {
   }
 };
 
-const uploadFile1 = async () => {
-  const fileUpload1 = document.getElementById("fileInput1").files[0];
-  if (fileUpload1) {
-    var formData = new FormData();
-    const url = `/upload`;
-    const headers = {
-      "Content-Type": "multipart/form-data",
-      "Access-Control-Allow-Origin": "*",
-    };
-    formData.append("doc", fileUpload1);
-    const response = await axios.post(url, formData, headers);
-    console.log(response);
-  }
+const download = (e) => {
+  e.preventDefault();
+  // Remote URL for the file to be downloaded
+  const url = 'pftc00001.appspot.com/completed/';
+  const filename = req.file.originalname+'.pdf';
+
+  fetch(url)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const blobURL = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = blobURL;
+      a.style.display = 'none';
+
+      if (filename && filename.length) a.download = filename;
+      document.body.appendChild(a);
+      a.click();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
