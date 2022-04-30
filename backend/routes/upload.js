5c85e64d2f7237a15bc112b587ba301faa55ca92
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2022-04-30 16:42:27 +0200

    adding download button8

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -130,7 +130,7 @@ upload.route("/").post(imageUpload.single("image"), (req, res) => {
          storage.bucket("pftc00001.appspot.com").file("completed/" + req.file.originalname.substring
           (0, req.file.originalname.lastIndexOf(".")) + ".pdf").save(myBuffer);
       
-          console.log("https://storage.googleapis.com/pftc00001.appspot.com/completed/"+req.file.originalname+'.pdf');
+          console.log("https://storage.googleapis.com/pftc00001.appspot.com/completed/"+req.filename+'.pdf');
             //console.log(myBuffer);
    
         }).catch((err) => {
