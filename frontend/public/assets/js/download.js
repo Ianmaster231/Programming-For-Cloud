e9569e651da9a12c40269d062ea43fe47ef2de42
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2022-04-30 21:13:57 +0200

    fixing firestore data for userclient adding try 24

--- frontend/public/assets/js/download.js
+++ frontend/public/assets/js/download.js
@@ -1,43 +1,10 @@
-/*
-**
-** This is a simple code for downloading an object from Google Cloud Storage in NodeJS.
-** Before downloading, remember to set up a GOOGLE_APPLICATION_CREDENTIALS environment variable that points to your service-account.json key.
-**
-*/ 
+import { download } from "./upload.js.js";
 
-// import the Google Cloud Storage client library
-import { Storage } from "@google-cloud/storage"
-
-// define the Google Cloud Storage bucket name
-const bucketName = 'pftc00001.appspot.com';
-
-// define the path and name of Google Cloud Storage object to download
-const srcFilename = "completed/" + req.file.originalname;
-
-// define the destination folder of downloaded object
-const destFilename = "../" ;
-
-// create a client
-const storage = new Storage();
-
-// define the function for file download
-async function downloadFile() {
-
-    // passing the options
-    const options = {
-        destination: destFilename,
-    };
-
-    // download object from Google Cloud Storage bucket
-    await storage.bucket(bucketName).file(srcFilename).download(options);
-
-    // [optional] a good log can help you in debugging
-    console.log(
-        "The object " + srcFilename +
-        " coming from bucket " +  bucketName +
-        " has been downloaded to " + destFilename
-    );
-}
-
-// call the download function and be ready to catch errors
-downloadFile().catch(console.error);
+ downloadfilebtn = async (href) => {
+   const href1 = download;
+  };
+  
+  
+   
+  
+  
