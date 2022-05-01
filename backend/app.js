de1745944ef9d80ba3d36fe588dd2a7a0ad5930d
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2022-05-01 21:09:45 +0200

    fixing firestore data for userclient adding try 86

--- backend/app.js
+++ backend/app.js
@@ -77,6 +77,7 @@ app.use("/auth", auth);
 
 //route upload traffic to upload.js
 app.use("/upload", upload);
+//route home traffic home
 
 app.use("/home", home);
 
--- backend/routes/clean.js
+++ backend/routes/clean.js
@@ -3,6 +3,11 @@ import Express from "express";
 const clean = Express.Router();
 
 //cron.route("/").post((req,res) =>{
-
+ // publicMessage({
+     //   email:email,
+  //      filename: req.file.originalname,
+   //     url: r.metadata.mediaLink,
+   //     date: new Date().toUTCString(),
+   //   });
 //});
 export default clean;
