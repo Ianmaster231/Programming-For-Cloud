65c0743faa92ad9af7d62b2bb92c4fa7267acac7
Ian Zammit <ian.zammit.a100896@mcast.edu.mt>
2022-05-02 19:02:27 +0200

    fixing cron job

--- backend/routes/clean.js
+++ backend/routes/clean.js
@@ -1,13 +1,19 @@
 import Express from "express";
+import {Storage} from "@google-cloud/storage";
 
+const bucket = "pftc00001.appspot.com";
 const clean = Express.Router();
+const storage = new Storage({
+   projectId: "pftc00001",
+   keyFilename: "./key.json",
+ });
 
-//cron.route("/").post((req,res) =>{
- // publicMessage({
-     //   email:email,
-  //      filename: req.file.originalname,
-   //     url: r.metadata.mediaLink,
-   //     date: new Date().toUTCString(),
-   //   });
-//});
+ clean.route("/").post(async (req,res) => {
+   const [filesContent] = await storage.bucket(bucket).getFiles();
+   filesContent.forEach(files => {
+       if(new Date(files.metadata.timeCreated) < Date.now() - (1000000)){
+         files.delete();
+       }
+   });
+});
 export default clean;
